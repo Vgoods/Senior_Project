2062faa99d8c3895d74da7c6393295c15ce11bcb
Vgoods <88050046+Vgoods@users.noreply.github.com>
2024-04-09 17:26:43 -0400

    Create SAA_Login_Connect.php

---
+++ Source/Save_An_Aggie_Login/SAA_Login_Connect.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Database connection
+    $conn = new mysqli('localhost', 'root', '', 'test');
+    if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
+
+    // Prepare SQL statement to retrieve user data
+    $stmt = $conn->prepare("SELECT id FROM register WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $stmt_result = $stmt->get_result();
+
+    if ($stmt_result->num_rows > 0) {
+        $data = $stmt_result->fetch_assoc();
+        $_SESSION['user_id'] = $data['id'];
+        echo json_encode(array("message" => "Login Successfully"));
+    } else {
+        echo json_encode(array("error" => "Invalid Email or Password"));
+    }
+
+    $stmt->close();
+    $conn->close();
+?>
