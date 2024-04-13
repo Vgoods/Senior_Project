00f13d48c008147c0455ffe3241e3f6cd8980eab
Vgoods <88050046+Vgoods@users.noreply.github.com>
2024-04-12 20:46:52 -0400

    Update SAA_Request.js

--- Source/Save_An_Aggie_Request/SAA_Request.js
+++ Source/Save_An_Aggie_Request/SAA_Request.js
@@ -3,30 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
     var emailInput = document.getElementById("request-email");
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
 
         var emailValue = emailInput.value.trim();
-        var emailPattern = /@aggies\.ncat\.edu$/; // Regex pattern for email validation
+        var emailPattern = /@aggies\.ncat\.edu$/; 
 
         if (!emailPattern.test(emailValue)) {
             alert("Please enter a valid Aggie email address (ending with @aggies.ncat.edu).");
             return;
         }
 
-        // If email is valid, submit the form
-        var formData = new FormData(form); // Create a FormData object with form data
+        var formData = new FormData(form); 
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", form.action, true); // Open a POST request to form action asynchronously
+        xhr.open("POST", form.action, true); 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                // If request was successful, display alert and reload page
                 alert("Your request has been submitted.");
                 window.location.reload();
             } else {
-                // If there was an error, display an error message
                 alert("Error occurred while submitting the request.");
             }
         };
-        xhr.send(formData); // Send form data asynchronously
+        xhr.send(formData); 
     });
 });
